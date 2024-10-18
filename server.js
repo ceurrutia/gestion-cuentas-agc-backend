09e1da621893579ec879a2204b0dcaf2840239e8
Cecilia urrutia <urrutia.cici@gmail.com>
2024-10-18 15:57:49 -0300

    Connfig intetos de conexion a ddbb

--- server.js
+++ server.js
@@ -19,6 +19,23 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.log('Error al conectar con MongoDB:', err));
 
+const connectWithRetry = () => {
+        console.log('Intentando conectar a MongoDB...');
+        mongoose.connect(process.env.MONGO_URI, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000  
+        })
+        .then(() => console.log('Conectado a MongoDB'))
+        .catch(err => {
+            console.error('Error al conectar a MongoDB:', err);
+            console.log('Reintentando en 5 segundos...');
+            setTimeout(connectWithRetry, 5000);  
+        });
+    };
+    
+    connectWithRetry();
+
 // Rutas
 app.get('/cuentas', async (req, res) => {
     const nombreApellido = req.query.nombreApellido;
