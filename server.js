29fc87e344065f743c0d2772154863244a50893c
Cecilia urrutia <urrutia.cici@gmail.com>
2024-10-18 09:21:37 -0300

    agrego cors

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.log('Error al conectar con MongoDB:', err));
 
+app.use(cors({
+    origin: 'https://gestion-cuentas-agc-frontend-apktwvaot.vercel.app',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']  
+}));
+
 // Rutas
 // Toodas las cuentas
 app.get('/cuentas', async (req, res) => {
